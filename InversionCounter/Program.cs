3c3f036a31f7e43b95efbe7b9a9db0ff41eca34c
wzym@yandex.ru <Afdqqq*___92>
2019-10-04 15:10:13 +0300

    Works: it counts correctly. By mergeSort and recursion.

--- InversionCounter/Program.cs
+++ InversionCounter/Program.cs
@@ -5,18 +5,24 @@ namespace InversionCounter
 {
     internal class Program
     {
+        private static long _inversionCounter = 0;
+        
         public static void Main()
         {
-            var queue = GetInArray();
-            Console.WriteLine(CountInversions(queue));
+            var arrayToCount = GetArray();
+            SortAndCount(arrayToCount);
+            Console.WriteLine(_inversionCounter);
         }
 
-        private static int CountInversions(Queue<int[]> q)
+        private static int[] SortAndCount(int[] arrayToCount)
         {
-            while (q.Count > 1)
-                q.Enqueue(Merge(q.Dequeue(), q.Dequeue()));
-            
-            return 0;
+            if (arrayToCount.Length == 1) return arrayToCount;
+            var middle = arrayToCount.Length / 2;
+            var left = new int[middle];
+            var right = new int[arrayToCount.Length - middle];
+            Array.Copy(arrayToCount, 0, left, 0, left.Length);
+            Array.Copy(arrayToCount, middle, right, 0, right.Length);
+            return Merge(SortAndCount(left), SortAndCount(right));
         }
 
         private static int[] Merge(IReadOnlyList<int> a, IReadOnlyList<int> b)
@@ -28,23 +34,28 @@ namespace InversionCounter
 
             while (resPointer < result.Length)
             {
-                result[resPointer++] =
-                    (aPointer == a.Count ||
-                     (bPointer < b.Count && b[bPointer] <= a[aPointer]))
-                        ? b[bPointer++]
-                        : a[aPointer++];
+                if (bPointer == b.Count ||
+                    (aPointer < a.Count && a[aPointer] <= b[bPointer]))
+                {
+                    result[resPointer++] = a[aPointer++];
+                }
+                else
+                {
+                    result[resPointer++] = b[bPointer++];
+                    _inversionCounter += a.Count - aPointer;
+                }
             }
 
             return result;
         }
 
-        private static Queue<int[]> GetInArray()
+        private static int[] GetArray()
         {
             var size = int.Parse(Console.ReadLine());
             var arrayAsSubstrings = Console.ReadLine().Split();
-            var result = new Queue<int[]>();
+            var result = new int[size];
             for (var i = 0; i < size; i++)
-                result.Enqueue(new[] {int.Parse(arrayAsSubstrings[i])});
+                result[i] = int.Parse(arrayAsSubstrings[i]);
 
             return result;
         }
