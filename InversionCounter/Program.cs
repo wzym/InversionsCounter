2e168c2e1127ab660b1cd738ddfbd994ac012a35
wzym@yandex.ru <Afdqqq*___92>
2019-10-04 13:34:05 +0300

    MergeSort works.

--- InversionCounter/Program.cs
+++ InversionCounter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace InversionCounter
 {
@@ -6,25 +7,44 @@ namespace InversionCounter
     {
         public static void Main()
         {
-            var arrayToCount = GetInArray();
-            Console.WriteLine(CountInversions(arrayToCount));
+            var queue = GetInArray();
+            Console.WriteLine(CountInversions(queue));
         }
 
-        private static int CountInversions(int[] arrayToCount)
+        private static int CountInversions(Queue<int[]> q)
         {
+            while (q.Count > 1)
+                q.Enqueue(Merge(q.Dequeue(), q.Dequeue()));
             
             return 0;
         }
 
-        private static int[] GetInArray()
+        private static int[] Merge(IReadOnlyList<int> a, IReadOnlyList<int> b)
+        {
+            var result = new int[a.Count + b.Count];
+            var resPointer = 0;
+            var aPointer = 0;
+            var bPointer = 0;
+
+            while (resPointer < result.Length)
+            {
+                result[resPointer++] =
+                    (aPointer == a.Count ||
+                     (bPointer < b.Count && b[bPointer] <= a[aPointer]))
+                        ? b[bPointer++]
+                        : a[aPointer++];
+            }
+
+            return result;
+        }
+
+        private static Queue<int[]> GetInArray()
         {
             var size = int.Parse(Console.ReadLine());
             var arrayAsSubstrings = Console.ReadLine().Split();
-            var result = new int[size];
+            var result = new Queue<int[]>();
             for (var i = 0; i < size; i++)
-            {
-                result[i] = int.Parse(arrayAsSubstrings[i]);
-            }
+                result.Enqueue(new[] {int.Parse(arrayAsSubstrings[i])});
 
             return result;
         }
