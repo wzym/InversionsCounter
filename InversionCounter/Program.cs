912d48d4c53f186d28a146d6f9c97d660a67d4c7
wzym@yandex.ru <Afdqqq*___92>
2019-10-04 11:03:27 +0300

    Base of algorithm.

--- InversionCounter/Program.cs
+++ InversionCounter/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace InversionCounter
+﻿using System;
+
+namespace InversionCounter
 {
     internal class Program
     {
         public static void Main()
         {
+            var arrayToCount = GetInArray();
+            Console.WriteLine(CountInversions(arrayToCount));
+        }
+
+        private static int CountInversions(int[] arrayToCount)
+        {
             
+            return 0;
+        }
+
+        private static int[] GetInArray()
+        {
+            var size = int.Parse(Console.ReadLine());
+            var arrayAsSubstrings = Console.ReadLine().Split();
+            var result = new int[size];
+            for (var i = 0; i < size; i++)
+            {
+                result[i] = int.Parse(arrayAsSubstrings[i]);
+            }
+
+            return result;
         }
     }
 }
